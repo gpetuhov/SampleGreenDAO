fd91bc73b3b176c098a53332c633e5c2380f2145
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-06 17:55:30 +0300

    Insert notes into database

--- app/src/main/java/com/gpetuhov/android/samplegreendao/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplegreendao/MainActivity.kt
@@ -2,11 +2,23 @@ package com.gpetuhov.android.samplegreendao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var noteDao: NoteDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get DAO for notes
+        noteDao = (application as SampleGreenDaoApp).daoSession.noteDao
+
+        saveNoteButton.setOnClickListener {
+            val note = Note()
+            note.text = editText.text.toString()
+            noteDao.insert(note)
+        }
     }
 }
