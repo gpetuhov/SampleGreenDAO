8b6dd3322c98ec5d47cedbba04d399d7569f10f8
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-06 18:02:20 +0300

    Refactor MainActivity

--- app/src/main/java/com/gpetuhov/android/samplegreendao/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplegreendao/MainActivity.kt
@@ -2,23 +2,43 @@ package com.gpetuhov.android.samplegreendao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pawegio.kandroid.toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var noteDao: NoteDao
+    private var noteDao: NoteDao? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initNoteDao()
+
+        saveNoteButton.setOnClickListener { saveNote() }
+    }
+
+    private fun initNoteDao() {
         // Get DAO for notes
         noteDao = (application as SampleGreenDaoApp).daoSession.noteDao
+    }
 
-        saveNoteButton.setOnClickListener {
+    private fun saveNote() {
+        val text = editText.text.toString()
+
+        if (text != "") {
+            // Create new Note entity and insert it into database
             val note = Note()
-            note.text = editText.text.toString()
-            noteDao.insert(note)
+            note.text = text
+            noteDao?.insert(note)
+
+            // Clear EditText
+            editText.setText("")
+
+            toast("Note saved")
+
+        } else {
+            toast("Note should not be empty")
         }
     }
 }
