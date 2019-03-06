11baeeb2c8864217d8d9cf4f86cffeab30efb2a4
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-06 16:29:33 +0300

    Add empty project

---
+++ app/src/main/java/com/gpetuhov/android/samplegreendao/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gpetuhov.android.samplegreendao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
